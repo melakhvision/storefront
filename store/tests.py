fe426a4bc000680fc475366cb4613641214a07db
codekhalipha <melakhvision@gmail.com>
2023-08-21 20:01:48 +0000

    create product test

--- store/tests.py
+++ store/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
+from store.models import Product, Collection
 
 
 User = get_user_model()
+product = Product()
 client = Client()
 # Create your tests here.
 
@@ -22,6 +24,17 @@ class StoreTestCast(TestCase):
         # store self.user_a_pw to "thiane@1994" along the test and can be reused
         self.user_a_pwd = "thiane@1994"
 
+        product.title = "Macbook pro 2020"
+        product.slug = "mac-bok-pro-2020"
+        product.description = 'this is a macbook pro 2020 ready to use'
+        product.unit_price = 1452.10
+        # collection = Collection.objects.get(id=4)
+        product.collection = Collection(pk=1)
+        # print(collection)
+        product.inventory = 12
+        product.save()
+        self.product_id = product.id
+
     def test_user_exist(self):
         user_count = User.objects.all().count()
         self.assertEqual(user_count, 1)
@@ -38,3 +51,10 @@ class StoreTestCast(TestCase):
         user_a = user_qs.exists()
         self.assertTrue(user_a)
         self.assertTrue(self.user_a.check_password(self.user_a_pwd))
+
+    def test_create_product(self):
+        # product_id = self.product_id
+        print('running create test')
+        products = Product.objects.filter(id=self.product_id)
+        for product in products:
+            self.assertEqual(int(product.unit_price), 1452)
